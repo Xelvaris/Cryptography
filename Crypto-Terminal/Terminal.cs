b21141287d588aec546db2d7ef39fa266593ebf3
Mitchel Sykes <945mitchelsykes@gmail.com>
2019-10-21 16:20:42 -0400

    Basic Setup

---
+++ Crypto-Terminal/Terminal.cs
@@ -0,0 +1,19 @@
+﻿using CryptoLib;
+using System;
+
+namespace Crypto_Terminal
+{
+    class Terminal
+    {
+        public readonly string LogName = "CryptoTerminal.log";
+        static void Main(string[] args)
+        {
+            Arguments.ArgumentHandler(args);
+            Start();
+        }
+        private static void Start()
+        {
+
+        }
+    }
+}
