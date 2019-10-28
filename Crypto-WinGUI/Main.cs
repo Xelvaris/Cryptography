ec7447c6624eadaa979bb522e7bf3bbb19f7f778
Mitchel Sykes <945mitchelsykes@gmail.com>
2019-10-28 11:06:13 -0400

    Added Caesar cipher and some more basic setup

--- Crypto-WinGUI/Main.cs
+++ Crypto-WinGUI/Main.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using CryptoLib;
 
 namespace Crypto_WinGUI
 {
     public partial class Main : Form
     {
+        public readonly string LogName = "CryptoWIN.log";
         public Main()
         {
             InitializeComponent();
---
+++ CryptoLib/Caesar.cs
@@ -0,0 +1,44 @@
+﻿namespace CryptoLib
+{
+    class Caesar
+    {
+        public static string Encrypt(string input, int shift)
+        {
+            char[] buffer = input.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                letter = (char)(letter + shift);
+                if (letter > 'z')
+                {
+                    letter = (char)(letter - 26);
+                }
+                else if (letter < 'a')
+                {
+                    letter = (char)(letter + 26);
+                }
+                buffer[i] = letter;
+            }
+            return new string(buffer);
+        }
+        public static string Decrypt(string input, int shift)
+        {
+            char[] buffer = input.ToCharArray();
+            for (int i = 0; i < buffer.Length; i++)
+            {
+                char letter = buffer[i];
+                letter = (char)(letter - shift);
+                if (letter > 'z')
+                {
+                    letter = (char)(letter - 26);
+                }
+                else if (letter < 'a')
+                {
+                    letter = (char)(letter + 26);
+                }
+                buffer[i] = letter;
+            }
+            return new string(buffer);
+        }
+    }
+}
