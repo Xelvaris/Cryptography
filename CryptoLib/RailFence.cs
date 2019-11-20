b380f5a0773823cd81a67f0d3298a89c1232e794
Mitchel Sykes <945mitchelsykes@gmail.com>
2019-11-20 11:03:19 -0500

    Added affine, railfence, and vigenere

---
+++ CryptoLib/RailFence.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace CryptoLib
+{
+    public static class RailFence
+    {
+        public static string Encrypt(string text, int rails)
+        {
+            text = text.ToUpper();
+            text = Regex.Replace(text, @"[^A-Z0-9]", string.Empty);
+
+            var lines = new List<StringBuilder>();
+
+            for (int i = 0; i < rails; i++)
+                lines.Add(new StringBuilder());
+
+            int currentLine = 0;
+            int direction = 1;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                lines[currentLine].Append(text[i]);
+
+                if (currentLine == 0)
+                    direction = 1;
+                else if (currentLine == rails - 1)
+                    direction = -1;
+
+                currentLine += direction;
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            for (int i = 0; i < rails; i++)
+                result.Append(lines[i].ToString());
+
+            return result.ToString();
+        }
+
+        public static string Decrypt(string text, int rails)
+        {
+            text = text.ToUpper();
+            text = Regex.Replace(text, @"[^A-Z0-9]", string.Empty);
+
+            var lines = new List<StringBuilder>();
+
+            for (int i = 0; i < rails; i++)
+            {
+                lines.Add(new StringBuilder());
+            }
+
+            int[] linesLenght = Enumerable.Repeat(0, rails).ToArray();
+
+            int currentLine = 0;
+            int direction = 1;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                linesLenght[currentLine]++;
+
+                if (currentLine == 0)
+                    direction = 1;
+                else if (currentLine == rails - 1)
+                    direction = -1;
+
+                currentLine += direction;
+            }
+
+            int currentChar = 0;
+
+            for (int line = 0; line < rails; line++)
+            {
+                for (int c = 0; c < linesLenght[line]; c++)
+                {
+                    lines[line].Append(text[currentChar]);
+                    currentChar++;
+                }
+            }
+
+            StringBuilder result = new StringBuilder();
+
+            currentLine = 0;
+            direction = 1;
+
+            int[] currentReadLine = Enumerable.Repeat(0, rails).ToArray();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+
+                result.Append(lines[currentLine][currentReadLine[currentLine]]);
+                currentReadLine[currentLine]++;
+
+                if (currentLine == 0)
+                {
+                    direction = 1;
+                }
+                else if (currentLine == rails - 1)
+                {
+                    direction = -1;
+                }
+
+                currentLine += direction;
+            }
+
+            return result.ToString();
+
+        }
+
+    }
+}
---
+++ CryptoLib/Vigenere.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoLib
+{
+    class Vigenere
+    {
+        public static string Run(string inputText, string keyword, int mode)
+        { 
+            List<char> alphabet =
+                Enumerable.Range('a', 'z' - 'a' + 1)
+                .Select(x => (char)x).ToList();
+
+            char[][] tabulaRecta = new char['z' - 'a' + 1][];
+            for (int i = 0; i < tabulaRecta.Length; i++)
+            {
+                tabulaRecta[i] = alphabet.ToArray();
+                var first = alphabet.First();
+                alphabet.Remove(first);
+                alphabet.Insert(alphabet.Count, first);
+            }
+
+            if(mode == 1)
+            {
+                string cipherText = Encrypt(inputText, tabulaRecta, keyword);
+                return cipherText;
+            }
+            else if(mode == 2)
+            {
+                string decipherText = Decrypt(inputText, tabulaRecta, keyword);
+            }
+            return "Error: Invalid mode";  
+        }
+
+        private static string GrowToTextSize(int length, string keyword)
+        {
+            string result = keyword;
+
+            int idx = 0;
+            while (result.Length < length)
+            {
+                result += keyword[idx++];
+
+                if (idx >= length)
+                {
+                    idx = 0;
+                }
+            }
+
+            return result;
+        }
+
+        private static char[][] TransposeMatrix(char[][] matrix)
+        {
+            char[][] result = new char[matrix[0].Length][];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = new char[matrix.Length];
+            }
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    result[col][row] = matrix[row][col];
+                }
+            }
+
+            return result;
+        }
+
+        private static int IndexOf(char[] array, char toFind)
+        {
+            int result = -1;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == toFind)
+                {
+                    result = i;
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private static string Encrypt(
+            string clearText, char[][] tabulaRecta, string keyword)
+        {
+            string result = string.Empty;
+
+            keyword = GrowToTextSize(clearText.Length, keyword);
+
+            for (int i = 0; i < clearText.Length; i++)
+            {
+                int row = clearText[i] - 'a';
+                int col = keyword[i] - 'a';
+
+                result += tabulaRecta[row][col];
+            }
+
+            return result;
+        }
+
+        private static string Decrypt(
+            string cipherText, char[][] tabulaRecta, string keyword)
+        {
+            string result = string.Empty;
+
+            keyword = GrowToTextSize(cipherText.Length, keyword);
+            tabulaRecta = TransposeMatrix(tabulaRecta);
+
+            for (int i = 0; i < cipherText.Length; i++)
+            {
+                int row = keyword[i] - 'a';
+                int col = IndexOf(tabulaRecta[row], cipherText[i]);
+
+                result += tabulaRecta[0][col];
+            }
+
+            return result;
+        }
+    }
+}
