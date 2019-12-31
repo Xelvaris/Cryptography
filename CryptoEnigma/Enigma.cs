8dd4c574a6605e3fb28a5277ae2798dd2f4aa840
Mitchel Sykes <945mitchelsykes@gmail.com>
2019-12-31 15:35:20 -0500

    Started on the enigma

---
+++ CryptoEnigma/Enigma.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CryptoEnigma
+{
+    public class Enigma
+    {
+        //Based around Enigma M3 Army 1938
+
+        private const string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        private const string rotorI = "EKMFLGDQVZNTOWYHXUSPAIBRCJ";
+        private const string rotorII = "AJDKSIRUXBLHWTMCQGZNPYFVOE";
+        private const string rotorIII = "BDFHJLCPRTXVZNYEIWGAKMUSQO";
+        private const string rotorIV = "ESOVPZJAYQUIRHXLNFTGKDCMWB";
+        private const string rotorV = "VZBRGITYUPSDNHLXAWMJQOFECK";
+        private const string reflectorA = "EJMZALYXVBWFCRQUONTSPIKHGD";
+        private const string reflectorB = "YRUHQSLDPXNGOKMIEBFZCWVJAT";
+        private const string reflectorC = "FVPJIAOYEDRZXWGCTKUQSBNMHL";
+
+        private RotorsReflector[] rotors;
+        private RotorsReflector reflector;
+
+        private Dictionary<Char, Char> plugBoard;
+    }
+}
