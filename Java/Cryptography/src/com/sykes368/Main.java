f9cbeb535a5d5e59ca6eba145e6e837d38cd50ed
Mitchel Sykes <945mitchelsykes@gmail.com>
2020-11-24 15:47:44 -0500

    Reworked homepage into main, started CaesarPage and updated css

--- Java/Cryptography/src/com/sykes368/Main.java
+++ Java/Cryptography/src/com/sykes368/Main.java
@@ -1,19 +1,19 @@
 package com.sykes368;
 
-import com.sykes368.gui.*;
+import com.sykes368.gui.CaesarPage;
+import com.sykes368.gui.UI;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -22,8 +22,6 @@ import javafx.stage.Stage;
  * @since 1.0
  */
 public class Main extends Application {
-    private static String currentPage = "Home";
-
     @Override
     public void start(Stage primaryStage) {
         // Creates Home Button
@@ -34,18 +32,50 @@ public class Main extends Application {
         homeButton.getStyleClass().add("icon-button");
 
         // Creates current page label
-        Label pageLabel = new Label(currentPage);
+        Label pageLabel = new Label("Home");
         pageLabel.getStyleClass().add("page-label");
 
         // Creates the top bar
         HBox topBar = new HBox(homeButton, pageLabel);
         topBar.getStyleClass().add("topbar");
 
+
+        // Home Menu Buttons
+        Button[] buttons = new Button[20];
+        for (int i = 0; i < 20; i++) {
+            buttons[i] = new Button();
+            buttons[i].setMinSize(200,200);
+            buttons[i].getStyleClass().add("home-menu-button");
+        }
+
+        // Sets the button text
+        buttons[0].setText("Caesar");
+
+
+
+        FlowPane flowPane = new FlowPane();
+        flowPane.getStyleClass().add("pane");
+        flowPane.setVgap(5);
+        flowPane.setHgap(5);
+        flowPane.setPadding(new Insets(5));
+
+        for (Button e: buttons) {
+            flowPane.getChildren().add(e);
+        }
+
+
+        ScrollPane page = new ScrollPane(flowPane);
+        page.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        page.setFitToWidth(true);
+        page.setFitToHeight(true);
+        page.getStyleClass().add("pane");
+
         // Creates main layout.
-        VBox vBox = new VBox(topBar, HomePage.show(), byLine());
+        Node byLine = byLine();
+        VBox rootLayout = new VBox(topBar, page, byLine);
 
         // Creates background scene
-        Scene scene = new Scene(vBox, 1290, 720);
+        Scene scene = new Scene(rootLayout, 1290, 720);
         scene.getStylesheets().add(UI.CSS_PATH);
 
         // Initializes the stage
@@ -57,10 +87,18 @@ public class Main extends Application {
 
         // HomeButton Click method
         homeButton.setOnAction(e -> {
-            if(!currentPage.equals("Home")) {
-                vBox.getChildren().addAll(HomePage.show(), byLine());
-                currentPage = "Home";
+            if(!pageLabel.getText().equals("Home")) {
+                page.setContent(flowPane);
+                rootLayout.getChildren().add(byLine);
+                pageLabel.setText("Home");
             }
+        });
+
+        // Caesar Button Click Method
+        buttons[0].setOnAction(e -> {
+            page.setContent(CaesarPage.show());
+            rootLayout.getChildren().remove(2);;
+            pageLabel.setText("Caesar");
         });
     }
 
--- Java/Cryptography/src/com/sykes368/gui/CaesarPage.java
+++ Java/Cryptography/src/com/sykes368/gui/CaesarPage.java
@@ -1,9 +1,15 @@
 package com.sykes368.gui;
 
+import com.sykes368.lib.Caesar;
 import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.FlowPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+
 
 /** Main
  * @author Sykes368
@@ -12,13 +18,65 @@ import javafx.scene.layout.VBox;
 public class CaesarPage {
     public static Pane show() {
 
-        VBox uiControls = new VBox();
+        // Shift
+        Label shiftLabel = new Label("Shift: ");
+        shiftLabel.getStyleClass().add("box-label");
+        TextField shiftNum = new TextField();
+        shiftNum.setPromptText("Enter a Number.");
+
+        HBox shift = new HBox(shiftLabel, shiftNum);
+
+        // Input
+        Label inputLabel = new Label("Input: ");
+        inputLabel.getStyleClass().add("box-label");
+        TextField inputBox = new TextField();
+        inputBox.setPrefWidth(500);
+        inputBox.setPromptText("Enter text to encrypt or decrypt.");
+
+        VBox input = new VBox(inputLabel, inputBox);
+
+        // Output
+        Label outputLabel = new Label("Output: ");
+        outputLabel.getStyleClass().add("box-label");
+        TextField outputBox = new TextField();
+        outputBox.setPrefWidth(500);
+        outputBox.setPromptText("...");
+        outputBox.setDisable(true);
+
+        VBox output = new VBox(outputLabel, outputBox);
+
+        // Control Buttons
+        Button encrypt = new Button("Encrypt");
+
+        Button decrypt = new Button("Decrypt");
+
+        HBox buttons = new HBox(encrypt, decrypt);
+        buttons.getStyleClass().add("");
+        buttons.setSpacing(20);
+
+        encrypt.setOnAction(e -> {
+            if (!shiftNum.getText().matches("^[0-9]+$")) {
+                outputBox.setText("[ERROR]: The shift value must be a number");
+            } else {
+                int s = Integer.parseInt(shiftNum.getText());
+                outputBox.setText(Caesar.encrypt(s, inputBox.getText()));
+            }
+        });
+
+
+
+
+        VBox uiControls = new VBox(shift, input, output, buttons);
+        uiControls.setSpacing(10);
 
         // Creates flowPane with padding of 5
+        // Required to add info page and challenge in future.
         FlowPane flowPane = new FlowPane();
+        flowPane.getStyleClass().add("pane");
         flowPane.setHgap(5);
         flowPane.setVgap(5);
         flowPane.setPadding(new Insets(5));
+        flowPane.getChildren().addAll(uiControls);
 
         return flowPane;
     }
